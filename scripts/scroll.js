e23af989634d6e6e1c4372cff744fc508d0617e5
davidrleonard <dleonard@codeforamerica.org>
2015-06-22 16:25:07 -0700

    Add Google Analytics, add URL tracking, fix scroll script breaking link tracking, closes #25

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,15 +1,11 @@
-// thanks to to https://css-tricks.com/snippets/jquery/smooth-scrolling/
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-});
+$('.js-smooth-scroll').click(function(e){
+    var that = this;
+    var target = $(that.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        return false;
+    };
+})''
